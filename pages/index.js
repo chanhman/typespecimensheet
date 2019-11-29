437f36e3266e82a7aaac1f0525c3aa6233db4e63
Chanh Nguyen <chanhthedj@gmail.com>
2019-11-28 19:59:00 -0700

    yay

--- pages/index.js
+++ pages/index.js
@@ -45,48 +45,34 @@ const Home = ({ nyt }) => {
         ))}
       </div>
 
-      <div className={css.grid3}>
-        <div>
-          <div className={css.grid2}>
-            {nyt.slice(0, 2).map(article => (
-              <div key={uuid()}>
-                <h2>{article.title}</h2>
-                <h4>{article.section}</h4>
-                <p>{article.abstract}</p>
-              </div>
-            ))}
+      <div className={css.grid2}>
+        {nyt.slice(0, 2).map(article => (
+          <div key={uuid()}>
+            <h2>{article.title}</h2>
+            <h4>{article.section}</h4>
+            <p>{article.abstract}</p>
           </div>
+        ))}
+      </div>
 
-          <div className={css.grid2}>
-            {nyt.slice(2, 6).map(article => (
-              <div key={uuid()}>
-                <h3>{article.title}</h3>
-                <h5>{article.section}</h5>
-                <p>{article.abstract}</p>
-              </div>
-            ))}
+      <div className={css.grid2}>
+        {nyt.slice(2, 6).map(article => (
+          <div key={uuid()}>
+            <h3>{article.title}</h3>
+            <h5>{article.section}</h5>
+            <p>{article.abstract}</p>
           </div>
+        ))}
+      </div>
 
-          <div className={css.grid2}>
-            {nyt.slice(6, 12).map(article => (
-              <div key={uuid()}>
-                <h4>{article.title}</h4>
-                <h6>{article.section}</h6>
-                <p>{article.abstract}</p>
-              </div>
-            ))}
+      <div className={css.grid2}>
+        {nyt.slice(6, 12).map(article => (
+          <div key={uuid()}>
+            <h4>{article.title}</h4>
+            <h6>{article.section}</h6>
+            <p>{article.abstract}</p>
           </div>
-        </div>
-
-        <aside>
-          {nyt.slice(12, 15).map(article => (
-            <div className={css.article} key={uuid()}>
-              <h4>{article.title}</h4>
-              <h6>{article.section}</h6>
-              <p>{article.abstract}</p>
-            </div>
-          ))}
-        </aside>
+        ))}
       </div>
     </div>
   )
