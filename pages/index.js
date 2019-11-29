36c127d6b8f61bd1ade869002d711febbd8cacf7
Chanh Nguyen <chanhthedj@gmail.com>
2019-11-28 18:11:09 -0700

    ☝️

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,101 @@
 import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import fetch from 'isomorphic-unfetch'
+import uuid from 'react-uuid'
+import css from "./index.scss"
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = ({ nyt }) => {
+  console.log(nyt);
 
-    <Nav />
+  return (
+    <div className={css.container}>
+      <header className={css.typefaceName}>
+        <h1>Title</h1>
+        <p>Read the news while testing out a typeface.</p>
+      </header>
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+      <div className={css.typefaceBasics}>
+        <h1>
+          A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+        </h1>
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+        <h1>
+          a b c d e f g h i j k l m n o p q r s t u v w x y z
+        </h1>
+
+        <h1>
+          1 2 3 4 5 6 7 8 9 0
+        </h1>
+      </div>
+
+      <div className={css.grid1}>
+        {nyt.slice(18, 20).map(article => (
+          <div key={uuid()}>
+            <h1>{article.title}</h1>
+            <p>{article.abstract}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className={css.grid2}>
+        {nyt.slice(15, 18).map(article => (
+          <div key={uuid()}>
+            <h5>{article.title}</h5>
+            <p>{article.abstract}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className={css.grid3}>
+        <div>
+          <div className={css.grid2}>
+            {nyt.slice(0, 2).map(article => (
+              <div key={uuid()}>
+                <h2>{article.title}</h2>
+                <h4>{article.section}</h4>
+                <p>{article.abstract}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className={css.grid2}>
+            {nyt.slice(2, 6).map(article => (
+              <div key={uuid()}>
+                <h3>{article.title}</h3>
+                <h5>{article.section}</h5>
+                <p>{article.abstract}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className={css.grid2}>
+            {nyt.slice(6, 12).map(article => (
+              <div key={uuid()}>
+                <h4>{article.title}</h4>
+                <h6>{article.section}</h6>
+                <p>{article.abstract}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <aside>
+          {nyt.slice(12, 15).map(article => (
+            <div className={css.article} key={uuid()}>
+              <h4>{article.title}</h4>
+              <h6>{article.section}</h6>
+              <p>{article.abstract}</p>
+            </div>
+          ))}
+        </aside>
       </div>
     </div>
+  )
+}
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+Home.getInitialProps = async ({ req }) => {
+  const res = await fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=8dgk2NGTAm0Aiulv7Td0dLnmAbvLvGAx')
+  const json = await res.json()
+  return { nyt: json.results }
+}
 
 export default Home
