cf764fa5d3c128dd0f9be0a8c393497e0f604d4c
Chanh Nguyen <chanhthedj@gmail.com>
2019-12-01 22:10:24 -0700

    Create an editor component

--- components/Editor.js
+++ components/Editor.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import classes from 'classnames'
 
+import HeadingEditor from './HeadingEditor'
 import css from './Editor.scss'
 
 const Editor = ({ editor }) => (
   <div className={classes(css.editor, editor ? css.editorDisplay : '')}>
-    Yay
+    <HeadingEditor name='H1' />
+    <HeadingEditor />
   </div>
 )
 
---
+++ components/HeadingEditor.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+
+import css from './HeadingEditor.scss'
+
+const HeadingEditor = ({ name }) => (
+  <div className={css.container}>
+    <div className={css.name}>{name}</div>
+    <div className={css.values}>
+      <div className={css.value}>
+        <label className={css.label} for='font-size'>
+          Font size:
+        </label>
+        <input className={css.input} type='number' id='font-size' />
+        <div className={css.unit}>px</div>
+      </div>
+      <div className={css.value}>
+        <label className={css.label} for='top-margin'>
+          Top margin:
+        </label>
+        <input className={css.input} type='number' id='top-margin' />
+        <div className={css.unit}>px</div>
+      </div>
+      <div className={css.value}>
+        <label className={css.label} for='bottom-margin'>
+          Bottom margin:
+        </label>
+        <input className={css.input} type='number' id='bottom-margin' />
+        <div className={css.unit}>px</div>
+      </div>
+    </div>
+  </div>
+)
+
+HeadingEditor.defaultProps = {
+  name: 'Heading'
+}
+
+HeadingEditor.propTypes = {
+  name: PropTypes.string
+}
+
+export default HeadingEditor
