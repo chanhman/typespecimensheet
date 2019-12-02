063d623ea052e24d87fb4647914b90b08bd75b0f
Chanh Nguyen <chanhthedj@gmail.com>
2019-12-01 22:37:36 -0700

    fix errors

--- components/HeadingEditor.js
+++ components/HeadingEditor.js
@@ -9,21 +9,21 @@ const HeadingEditor = ({ name }) => (
     <div className={css.name}>{name}</div>
     <div className={css.values}>
       <div className={css.value}>
-        <label className={css.label} for='font-size'>
+        <label className={css.label} htmlFor='font-size'>
           Font size:
         </label>
         <input className={css.input} type='number' id='font-size' />
         <div className={css.unit}>px</div>
       </div>
       <div className={css.value}>
-        <label className={css.label} for='top-margin'>
+        <label className={css.label} htmlFor='top-margin'>
           Top margin:
         </label>
         <input className={css.input} type='number' id='top-margin' />
         <div className={css.unit}>px</div>
       </div>
       <div className={css.value}>
-        <label className={css.label} for='bottom-margin'>
+        <label className={css.label} htmlFor='bottom-margin'>
           Bottom margin:
         </label>
         <input className={css.input} type='number' id='bottom-margin' />
--- components/Headings.js
+++ components/Headings.js
@@ -1,43 +1,44 @@
 import React from 'react'
 
 import PropTypes from 'prop-types'
+import uuid from 'react-uuid'
 
 import css from './Headings.scss'
 
 const Headings = ({ data }) => (
   <div>
     {data.slice(12, 13).map(article => (
-      <h1 className={css.heading}>
+      <h1 className={css.heading} key={uuid()}>
         <span>Heading 1</span>
         {article.title}
       </h1>
     ))}
     {data.slice(13, 14).map(article => (
-      <h2 className={css.heading}>
+      <h2 className={css.heading} key={uuid()}>
         <span>Heading 2</span>
         {article.title}
       </h2>
     ))}
     {data.slice(14, 15).map(article => (
-      <h3 className={css.heading}>
+      <h3 className={css.heading} key={uuid()}>
         <span>Heading 3</span>
         {article.title}
       </h3>
     ))}
     {data.slice(15, 16).map(article => (
-      <h4 className={css.heading}>
+      <h4 className={css.heading} key={uuid()}>
         <span>Heading 4</span>
         {article.title}
       </h4>
     ))}
     {data.slice(16, 17).map(article => (
-      <h5 className={css.heading}>
+      <h5 className={css.heading} key={uuid()}>
         <span>Heading 5</span>
         {article.title}
       </h5>
     ))}
     {data.slice(17, 18).map(article => (
-      <h6 className={css.heading}>
+      <h6 className={css.heading} key={uuid()}>
         <span>Heading 6</span>
         {article.title}
       </h6>
